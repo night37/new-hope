3e3213a660ff4b48c16bee104d40827355806959
Guillaume <guillaume@e-major.fr>
2022-09-04 19:03:08 +0200

    ajout composant search

---
+++ src/Form/AnimalType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Animal;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnimalType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('race')
+            ->add('espece')
+            ->add('gabarit')
+            ->add('sexe')
+            ->add('age')
+            ->add('pelage')
+            ->add('ville_adoption')
+            ->add('cas_specifique')
+            ->add('photos')
+            ->add('association_id')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Animal::class,
+        ]);
+    }
+}
