c4738316d25476d2ef4151245b9d24851d92ecc1
Guillaume Garnier <guillaume.garnier@inconito.fr>
2025-03-31 20:53:19 +0200

    feat: login and logout

--- back/src/Controller/AnimalController.php
+++ back/src/Controller/AnimalController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/animal')]
+#[Route('/animal', name: 'animal_index')]
 final class AnimalController extends AbstractController
 {
     #[Route('/api/getAnimalsList',name: 'get_animals_list', methods: ['GET'])]
