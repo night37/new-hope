e563cd7f5e1c6c39a80aac2a5965f36bbcf0cf42
Guillaume Garnier <guillaume.garnier@inconito.fr>
2025-04-27 15:48:54 +0200

    fix: some bug on login

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -9,42 +9,44 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Repository\UserRepository;
 use App\Security\EmailVerifier;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mime\Address;
-
+use App\Service\EmailService;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/connexion', name: 'app_login')]  // The login route                                                                  
-    public function login(AuthenticationUtils $authenticationUtils, Security $security): Response
+    private $authenticationUtils;
+    private $security;
+    private $emailService;
+
+
+    public function __construct(
+        AuthenticationUtils $authenticationUtils, 
+        Security $security,
+        EmailService $emailService  
+    ) {
+
+        $this->authenticationUtils = $authenticationUtils;
+        $this->security = $security;
+        $this->emailService = $emailService;
+    }
+
+
+    #[Route(path: '/connexion', name: 'app_login')]                                                             
+    public function login(): Response
     {
         if ($this->getUser()) {
-            if($this->getUser()->isVerified() === false) {
-                // If the user is not verified, log them out and redirect to the login route
-                
-                $request = $this->container->get('request_stack')->getCurrentRequest();
-                $email = $this->getUser()->getEmail();
-                $this->container->get('security.token_storage')->setToken(null);
-                $resendLink = $this->generateUrl('app_resend_verification', ['email' => $email]);
-                $request->getSession()->invalidate();
-                $this->addFlash('danger', 'Pour confirmer votre adresse e-mail, veuillez cliquer sur le lien contenu dans l\'e-mail que nous vous avons envoyé. Si vous n\'avez pas reçu cet e-mail ou si le lien a expiré,  <a href="' . $resendLink . '">cliquez ici</a> pour demander l\'envoi d\'un nouvel e-mail de confirmation.');
-
-                return $this->redirectToRoute('app_login');          
-                
-            }else{
-
-                $id = $this->getUser()->getId();
-                return $this->redirect('/user/'.$id.'/edit');    
+            $response = $this->emailService->verifyEmail($this->getUser(), $this->getUser()->getEmail());
+            if ($response) {
+                return $response;
             }
         }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+    
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $this->authenticationUtils->getLastUsername();
         return $this->render('security/login.html.twig', ['email' => $lastUsername, 'error' => $error]);
     }
+
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
@@ -52,44 +54,11 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
-    #[Route(path: '/resend-verification', name: 'app_resend_verification')]
-public function resendVerificationEmail(Request $request, UserRepository $userRepository, EmailVerifier $emailVerifier): Response
-{
-    $email = $request->query->get('email');
-    
-    if (!$email) {
-        $this->addFlash('danger', 'Aucune adresse email fournie.');
-        return $this->redirectToRoute('app_login');
+    #[Route(path: '/resend-verification-email-link', name: 'app_resend_verification_email_link')]
+    public function resendVerificationEmailLink(Request $request, UserRepository $userRepository, EmailVerifier $emailVerifier): Response
+    {
+        return $this->emailService->resendVerificationEmail($request, $userRepository, $emailVerifier);
     }
-    
-    $user = $userRepository->findOneBy(['email' => $email]);
-    
-    if (!$user) {
-        $this->addFlash('danger', 'L\'adresse email n\'est pas associée à un compte.');
-        return $this->redirectToRoute('app_login');
-    }
-    
-    if ($user->isVerified()) {
-        $this->addFlash('info', 'Votre compte est déjà vérifié. Vous pouvez vous connecter.');
-        return $this->redirectToRoute('app_login');
-    }
-    
-    // Création du nouvel email de vérification
-    $email = (new TemplatedEmail())
-        ->from(new Address('no-reply@votre-site.com', 'Votre Site'))
-        ->to($user->getEmail())
-        ->subject('Confirmation de votre adresse email')
-        ->htmlTemplate('registration/confirmation_email.html.twig');
-    
-    // Utilisation de votre EmailVerifier
-    $emailVerifier->sendEmailConfirmation(
-        'app_verify_email', // le nom de la route de vérification
-        $user,
-        $email
-    );
-    
-    $this->addFlash('success', 'Un nouvel email de confirmation a été envoyé. Veuillez vérifier votre boîte de réception.');
-    return $this->redirectToRoute('app_login');
-}
+
 }
 
