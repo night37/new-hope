984e69b019ff49534816b07a8ee29d7ae7aa99f9
Guillaume Garnier <guillaume.garnier@inconito.fr>
2025-05-04 18:21:55 +0200

    feat: add faker

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -23,4 +23,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
