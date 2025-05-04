3579cb7623ded5a590754d997ebc5f68210b49f0
Guillaume Garnier <guillaume.garnier@inconito.fr>
2025-05-04 17:02:10 +0200

    feat: api plateform init

--- back/src/Entity/User.php
+++ back/src/Entity/User.php
@@ -3,16 +3,25 @@
 namespace App\Entity;
 
 use App\Enum\Role;
+use ApiPlatform\Metadata\Get;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity('email', message: 'Cet email est déjà utilisé. Veuillez en choisir un autre.')]
+#[ApiResource (
+    operations: [
+        new Get(),
+        new GetCollection()
 
+    ]
+)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
