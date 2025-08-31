8764ee8ffc92b068108a805506d49c00f2fa6646
guillaume <garnier.guillaume37550@gmail.com>
2025-08-31 12:09:22 +0200

    fix ci

--- back/src/Repository/AnimalRepository.php
+++ back/src/Repository/AnimalRepository.php
@@ -16,9 +16,44 @@ class AnimalRepository extends ServiceEntityRepository
         parent::__construct($registry, Animal::class);
     }
 
-//    /**
-//     * @return Animal[] Returns an array of Animal objects
-//     */
+  /**
+    * @return Animal[] Returns an array of Animal objects
+ */
+        public function findAll(): array
+        {
+            return $this->createQueryBuilder('animal')
+                ->orderBy('animal.id', 'ASC')
+                ->Where('animal.isActive = true')
+                ->Where('animal.isVisible = true')
+                ->select($this->fieldsList())
+                ->getQuery()
+                ->getResult()
+            ;
+        }
+
+        private function fieldsList(): array
+        {
+            return [
+                'animal.name',
+                'animal.gender',
+                'animal.age',
+                'animal.out_department',
+                'animal.id',
+                'animal.highlight',
+                'animal.createdAt',
+                'animal.updatedAt',
+                'animal.size',
+                'animal.color',
+                'animal.affinity',
+                'animal.adoption_status',
+                'animal.breed',
+                'animal.type',
+                'animal.thumbnail',
+                'animal.images',
+                'animal.description',
+            ];
+        }
+
 //    public function findByExampleField($value): array
 //    {
 //        return $this->createQueryBuilder('a')
