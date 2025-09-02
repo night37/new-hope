d15faab45cf96f3b92aca0c9459fc2a22a19684e
Guillaume <garnier.guillaume37550@gmail.com>
2025-09-02 15:51:16 +0200

    footer

--- front/app/Components/header/Header.tsx
+++ front/app/Components/header/Header.tsx
@@ -1,9 +1,9 @@
 "use client"
-import { useState, useEffect } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Menu } from './Menu'
-import './style.scss'
+import { useState, useEffect } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { Menu } from "./Menu"
+import "./style.scss"
 
 
 type HeaderProps = {
@@ -17,22 +17,21 @@ export function Header ({font}: HeaderProps) {
 
   useEffect(() => {
     if (isOpen) {
-      document.body.style.overflow = 'hidden'
+      document.body.style.overflow = "hidden"
     } else {
-      document.body.style.overflow = 'unset'
+      document.body.style.overflow = "unset"
     }
 
-    // Cleanup function pour remettre le scroll quand le composant se démonte
     return () => {
-      document.body.style.overflow = 'unset'
+      document.body.style.overflow = "unset"
     }
   }, [isOpen])
 
     return(
         <header className={`${font.className} bg-secondary w-full`}>
           <div className={`${font.className} text-l container mx-auto py-[16.5px] grid items-center grid-cols-12`}> 
-            <div className='col-span-2 lg:col-span-3 xl:col-span-5'>
-              <Link href="/" className='z-10'>
+            <div className="col-span-2 lg:col-span-3 xl:col-span-5">
+              <Link href="/" className="z-10">
                   <Image
                     src="/logo.svg"
                     width={80}
@@ -41,16 +40,16 @@ export function Header ({font}: HeaderProps) {
                   />
               </Link>
             </div>
-            <div className='menu-container lg:col-span-9 xl:col-span-7 col-span-10 hidden lg:block'>
+            <div className="menu-container lg:col-span-9 xl:col-span-7 col-span-10 hidden lg:block">
               <Menu direction={"row"}/>
             </div>
-            <div className='menu-mobile-container lg:hidden col-span-10 flex justify-end'>
-              <button onClick={()=> setIsOpen(!isOpen)} className='z-10'>
+            <div className="menu-mobile-container lg:hidden col-span-10 flex justify-end">
+              <button onClick={()=> setIsOpen(!isOpen)} className="z-10">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
               </button>
-              <div className={` transition delay-150 duration-300 ease-in-out absolute left-0 ${isOpen ? "left-0 w-full" : " w-0 hidden left-full"} right-0 top-0 bottom-0  bg-secondary flex-col`}>
+              <div className={`transition delay-150 duration-300 ease-in-out absolute left-0 ${isOpen ? "left-0 w-full" : " w-0 hidden left-full"} right-0 top-0 bottom-0  bg-secondary flex-col`}>
                 <Menu direction={"col"}/>
               </div>
             </div>
