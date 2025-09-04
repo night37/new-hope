eeb09b3529e34c0e5a3b4a7f7651b8c64967b006
Guillaume <garnier.guillaume37550@gmail.com>
2025-09-04 14:56:12 +0200

    filter wip

--- back/tailwind.config.js
+++ back/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {      
+      fontFamily: {
+        caveat: 'var(--font-caveat)',
+        handlee: 'var(--font-handlee)'
+      },},
   },
   plugins: [],
 }
