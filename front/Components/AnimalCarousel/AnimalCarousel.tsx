eb2442161d4ca8bafb3d3686ee778b398e148020
Guillaume <garnier.guillaume37550@gmail.com>
2025-09-15 12:30:00 +0200

    carousel wip

--- front/Components/AnimalCarousel/AnimalCarousel.tsx
+++ front/Components/AnimalCarousel/AnimalCarousel.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import type { SwiperRef } from 'swiper/react';
 import { AnimalCard } from "@/Components/animalCard/AnimalCard"
 import { Navigation } from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import './style.scss';
@@ -10,26 +12,56 @@ interface AnimalItem {
     thumbnail: string;
     name: string
     breed: string,
-    gender: string,
-    associationName: string
+    gender: string,   
+    structure: {
+        id: string,
+        name: string
+    }
 }
 
 interface CarouselProps {
-    items: object[];
+    items: AnimalItem[];
 }
 
 
 function Carousel({ items}: CarouselProps) {
+    const swiperRef = useRef<SwiperRef | null>(null);
+
     console.log(items);
     return (
         <>
         <Swiper
-            navigation={true}
             modules={[Navigation]}   
+            ref={swiperRef}
             className="animal-swiper"
             slidesPerView={4}
             spaceBetween={30}
             centeredSlides={false}
+            keyboard={{
+                enabled: true,
+                onlyInViewport: true
+            }}
+            breakpoints= {{
+                    300: {
+                        slidesPerView: 1,
+                        spaceBetween: 40
+                    },
+                    618: {
+                        slidesPerView: 2,
+                        spaceBetween: 40
+                    },
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 40
+                    },
+                    1280: {
+                        slidesPerView: 4,
+                        spaceBetween: 40
+                    }
+
+            }}
+
+
         >
             {items.map((item, key) => (
                 <SwiperSlide key={key}>
@@ -49,6 +81,20 @@ function Carousel({ items}: CarouselProps) {
                 </SwiperSlide>
             ))}
         </Swiper>
+        <button 
+            className="custom-prev-btn"
+            onClick={() => swiperRef.current?.swiper.slidePrev()}
+        >
+            &#8249;
+        </button>
+      
+
+        <button 
+            className="custom-next-btn"
+            onClick={() => swiperRef.current?.swiper.slideNext()}
+        >
+            test
+        </button>
         
         </>
 
