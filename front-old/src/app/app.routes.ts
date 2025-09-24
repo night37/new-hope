34a3fb57f0089ec1cf933de32b624e8bf5e02885
guillaume <garnier.guillaume37550@gmail.com>
2025-09-24 21:03:58 +0200

    fix components name

--- front-old/src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component'; // Importez le composant ici !
-
-export const routes: Routes = [{ path: 'home', component: HomeComponent }];
