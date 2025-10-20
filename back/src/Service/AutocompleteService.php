b33eaf55d4c2acdc3a87b22e420f86e1a0e18b57
Guillaume <garnier.guillaume37550@gmail.com>
2025-10-20 16:56:19 +0200

    feat: center regions et departements

--- back/src/Service/AutocompleteService.php
+++ back/src/Service/AutocompleteService.php
@@ -1,17 +1,21 @@
 <?php
 namespace App\Service;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
 
 
 class AutocompleteService
 {
     private $geoApiUrl;
     private $client;
+    private $kernel;
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct(HttpClientInterface $client, KernelInterface $kernel)
     {
         $this->client = $client;
         $this->geoApiUrl = $_ENV['AUTOCOMPLETE_API_BASE_URL'];
+        $this->kernel = $kernel;
     }
 
     public function autocomplete (string $option, ?string $name, ?string $departement, ?string $region) : array {
@@ -19,7 +23,7 @@ class AutocompleteService
         if ($name ) {
             $query .= "nom=$name";
         }
-        $query .= "&limit=500&boost=population";
+        $query .= "&limit=500&boost=population&fields=code,nom,centre,codeRegion,codeDepartement";
 
         if ($region) {
             $query .= "&codeRegion=$region";
@@ -34,6 +38,7 @@ class AutocompleteService
             throw new \Exception('Erreur lors de la requête d\'autocomplétion');
         };
         $data = [];
+
         foreach ($response->toArray() as $key => $value) {
             $newData = [];
             $newData["name"] = $value["nom"];
@@ -41,12 +46,25 @@ class AutocompleteService
             if($option == 'communes'){
                 $newData["codeDepartement"] = $value["codeDepartement"];
                 $newData["codeRegion"] = $value["codeRegion"];
+                $newData["centre"] = $value["centre"];
             }
             if($option == 'departements'){
                 $newData["codeRegion"] = $value["codeRegion"];
+            }
+            if($option == 'departements' || $option == 'regions') {
+                $newData["center"] = $this->getCenterPosition($option, $value["nom"] );
             }
             array_push($data, $newData);
         }
         return $data;
     }
+
+
+
+    public function getCenterPosition (string $type, string $name): object {
+        $path = $this->kernel->getProjectDir() . $type == 'regions' ? '../assets/data/regions.json' : '../assets/data/departements.json';
+        
+        dd($type, $name);
+
+    } 
 }
