fd7442fb1a13401f807e4a4cdb4de68199695627
guillaume <garnier.guillaume37550@gmail.com>
2025-10-29 14:01:21 +0100

    fix: typo

--- front/app/structuresSearch/Map.tsx
+++ front/app/structuresSearch/Map.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, {useState, useEffect} from 'react';
 import { MapContainer, TileLayer, Popup, Marker, useMap } from 'react-leaflet';
 import { StructureListProps } from '@/types/structureListProps.type';
