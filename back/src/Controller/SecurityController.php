9c25f8a90d4c2499f0f6885ea25acbc599eb7e9c
guillaume <garnier.guillaume37550@gmail.com>
2025-12-15 22:38:48 +0100

    refactor security controller

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -3,35 +3,25 @@
 namespace App\Controller;
 
 use App\Service\EmailService;
-use App\Security\EmailVerifier;
 use App\Repository\UserRepository;
-use Symfony\Bundle\SecurityBundle\Security;
+use App\Service\FlashMessageService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class SecurityController extends AbstractController
 {
-    private $authenticationUtils;
-    private $security;
-    private $emailService;
+
 
 
     public function __construct(
-        AuthenticationUtils $authenticationUtils,
-        Security $security,
-        EmailService $emailService
-    ) {
-
-        $this->authenticationUtils = $authenticationUtils;
-        $this->security = $security;
-        $this->emailService = $emailService;
-    }
+        private AuthenticationUtils $authenticationUtils,
+        private EmailService $emailService,
+        private FlashMessageService $flashMessageService,
+    ) {}
 
     #[Route(path: '/')]
     public function index(): Response
@@ -44,7 +34,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/connexion', name: 'app_login')]
     public function login(Request $request): Response | RedirectResponse
     {
-        /** @var \App\Entity\User|null $user */
         $user = $this->getUser();
         $baseUrl = $request->getSchemeAndHttpHost();
         $basePath = $request->getBasePath();
@@ -57,7 +46,7 @@ class SecurityController extends AbstractController
         $lastUsername = $this->authenticationUtils->getLastUsername();
 
         if ($error) {
-            $this->addFlash('danger', 'Identifiants invalides.');
+            $this->flashMessageService->displayMessage('danger', 'Identifiants invalides.');
         }
 
         return $this->render('security/login.html.twig', [
